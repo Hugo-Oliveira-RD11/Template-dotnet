7a0a3b3709bced847aa7cb153e90236fe1532c0e
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2022-02-11 15:47:36 -0300

    Criando um novo Services para criar o sqlite

--- Crud/Program.cs
+++ Crud/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
